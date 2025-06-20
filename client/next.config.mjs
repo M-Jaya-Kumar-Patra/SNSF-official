83281eb1c06fd90db297ee1ad9b292b910a759e4
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2025-06-20 12:38:50 +0530

    api removed

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com'],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'res.cloudinary.com',
+    },
+  ],
+},
   env: {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
   },
