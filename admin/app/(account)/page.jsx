2c9756c9eac026d1b97659f99323ff1d8a8f6773
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2025-06-25 10:47:51 +0530

    Completed without Responsiveness 1 admin prd

--- admin/app/(account)/page.jsx
+++ admin/app/(account)/page.jsx
@@ -1,32 +1,23 @@
 "use client";
 
-import { useSession, signOut } from "next-auth/react";
 import Navbar from "@/components/Navbar";
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
-import { Admin, Package, MapPin, Heart, RefreshCcw, Bell, LifeBuoy, LogOut } from "lucide-react";
+import { Admin, Package, MapPin, Heart, RefreshCcw, Bell, LifeBuoy } from "lucide-react";
 import Link from "next/link";
-import { useAuth } from '../../context/AuthContext'; // ✅ corrected path if needed
+import { useAuth } from '../../context/AuthContext';
 import { useRouter } from "next/navigation";
 import LogoutBTN from "@/components/LogoutBTN";
 
 const Account = () => {
-  const { admin, logout, isLogin, adminData, setAdminData, isLoading, } = useAuth();
+  const { logout, isLogin, adminData } = useAuth();
   const router = useRouter();
-   const { jwtAdmin, setJwtAdmin } = useAuth();
+
   useEffect(() => {
     if (!isLogin) {
       router.push("/login");
     }
-  }, [isLogin]);
-
-  if (!admin) return <p className="text-center">Loading...</p>;
-
-  const handleLogout = async () => {
-    logout();
-    await signOut({ redirect: false });
-    router.push('/');
-  };
+  }, [isLogin, router]);
 
   if (!isLogin) {
     return <p className="text-center text-gray-500">Loading...</p>;
@@ -40,17 +31,16 @@ const Account = () => {
         <div className="left w-[250px] min-h-screen border-r-[1px] shadow-xl border-gray-300">
           <div className="p-3 pt-0 flex items-center gap-3 mt-4">
             <img
-                                className="h-[140px] w-[140px] rounded-full object-cover"
-                                src={adminData?.avatar || "/images/account.png"}
-                                alt="Admin Profile"
-                            />
+              className="h-[140px] w-[140px] rounded-full object-cover"
+              src={adminData?.avatar || "/images/account.png"}
+              alt="Admin Profile"
+            />
             <div className="flex flex-col items-start font-sans text-sm break-words">
               <h2 className="text-gray-700 text-base font-semibold">{adminData?.name}</h2>
             </div>
           </div>
 
           <ul className="font-sans text-gray-600">
-            {/* Menu Items */}
             <li><Link href="/profile"><div className="menu-item"><Admin size={18} /> Profile Information</div></Link></li>
             <li><Link href="/orders"><div className="menu-item"><Package size={18} /> My Orders</div></Link></li>
             <li><Link href="/account/address"><div className="menu-item"><MapPin size={18} /> Manage Address</div></Link></li>
