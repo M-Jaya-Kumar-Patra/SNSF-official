f7b3e2edb9da4ee65c1c01fa57e756f22544bd11
M-Jaya-Kumar-Patra <jayapatra2004@gmail.com>
2025-07-03 17:55:56 +0530

    Zoom

--- client/app/layout.js
+++ client/app/layout.js
@@ -49,6 +49,8 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="SNSF" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes" />
+
       </head>
 
       <body className={`${inter.className} w-full`}>
